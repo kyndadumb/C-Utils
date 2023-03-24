97c942e46083b3048a3b22f34c1062b31407fddf
kyndadumb <46259262+kyndadumb@users.noreply.github.com>
2023-03-24 20:03:42 +0100

    added Sieve of Eratosthenes

---
+++ SieveOfEratosthenes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SieveOfEratosthenes;
+
+class Program
+{
+    static void Main()
+    {
+        const int MAX = 100;
+
+        // Create an boolean array for indication if a number is prime
+        // start by setting all true
+
+        bool[] isPrime = new bool[MAX+1];
+
+        // for - all values in bool[] isPrime
+        for (int i = 0; i < isPrime.Length; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        // how does the sieve work?
+        // loop through number i --> start with 2 - each multiple of this isPrime = false
+        for (int i=2; i < Math.Sqrt(MAX)+1; i++) 
+        {
+            // if - isPrime
+            if (isPrime[i]) 
+            {
+                // for - all multiples of i
+                for (int j = (int)Math.Pow(i,2); j<=MAX; j += i) 
+                {
+                    // set isPrime to false
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // print results
+        
+        // for - each number until MAX
+        for (int k = 2; k <= MAX; k++)
+        {
+            // if k isPrime - print result
+            if (isPrime[k])
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
