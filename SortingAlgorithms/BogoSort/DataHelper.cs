c869776f4131156fc27ad4c2e3c44426fb7613ce
kyndadumb <46259262+kyndadumb@users.noreply.github.com>
2023-04-16 19:36:07 +0200

    init BogoSort (not working)

---
+++ SortingAlgorithms/BogoSort/DataHelper.cs
@@ -0,0 +1,51 @@
+﻿namespace BogoSort;
+public class DataHelper
+{
+    public static void GenerateInput()
+    {
+        Random rand = new Random();
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i <= 100; i++)
+        {
+            numbers.Add(rand.Next(1,1001));
+        }
+
+        String filepath = "input.txt";
+
+        using (StreamWriter writer = new StreamWriter(filepath))
+        {
+            foreach (int number in numbers)
+            {
+                writer.WriteLine(number);
+            }
+        }
+    }
+
+    public static List<int> ReadInput(String filename)
+    {
+        List<int> data = null;
+
+        try 
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                String line;
+                
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int number = int.Parse(line);
+                    data.Add(number);
+                }
+            }
+        }
+        
+        catch (Exception e) 
+        {
+            Console.WriteLine("Error while Reading the file: " + e.Message);
+        }
+
+        return data;
+    }
+}
---
+++ SortingAlgorithms/BogoSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System;
+
+namespace BogoSort;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create sample Data
+        DataHelper.GenerateInput();
+
+        // Read sample Data
+        List<int> numbers = DataHelper.ReadInput(".\\input.txt");
+
+        Console.WriteLine("BogoSort is sorting");
+
+        Sort(numbers);
+    }
+
+
+    // Check if the list is correctely sorted
+    static bool IsSorted(List<int> data)
+    {
+        for (int i = 0; i <= data.Count; i++)
+        {
+            if (data[i] > data[i + 1])
+            {
+                return false;
+            }
+        }        
+        
+        return true;
+    }
+
+    static void PrintIteration(List<int> data, int iteration)
+    {
+        Console.WriteLine("BogoSort iteration #{0}: ", iteration);
+
+        foreach (int value in data)
+        {
+            Console.WriteLine($"{value}");
+        }
+    }
+
+    static void Sort(List<int> data)
+    {
+        int iteration = 0;
+
+        while (!IsSorted(data))
+        {
+            PrintIteration(data, iteration);
+            data = Shuffle(data);
+            iteration++;
+        }
+
+        PrintIteration(data,iteration);
+        Console.WriteLine("BogoSort completed after {0} iterations.", iteration);
+    }
+
+    static List<int> Shuffle(List<int> numbers)
+    {
+        Random rand = new Random();
+
+        for (int i = numbers.Count - 1; i > 0; --i)
+        {
+            int k = rand.Next(i + 1);
+
+            int temp = numbers[i];
+            numbers[i] = numbers[k];
+            numbers[k] = temp;
+        }
+        
+        return numbers;
+    }
+}
